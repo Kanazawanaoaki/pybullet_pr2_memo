5c34ade3b1c7c5c67d0a0c09ca44699e7ab0502f
Kanazawanaoaki <naoaki65k@gmail.com>
2020-05-13 20:51:35 +0900

    test pr2 import actionlib

---
+++ test/wi_test.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import time
+
+import numpy as np
+import pybullet
+
+import skrobot
+
+
+def main():
+    # initialize robot
+    robot = skrobot.models.pr2.PR2()
+    interface = skrobot.interfaces.PR2ROSRobotInterface(robot)
+    pybullet.resetDebugVisualizerCamera(
+        cameraDistance=1.5,
+        cameraYaw=45,
+        cameraPitch=-45,
+        cameraTargetPosition=(0, 0, 0.5),
+    )
+    print('==> Initialized PR2 Robot on PyBullet')
+    for _ in range(100):
+        pybullet.stepSimulation()
+    time.sleep(3)
+
+    # reset pose
+    print('==> Moving to Reset Pose')
+    robot.reset_manip_pose()
+    interface.angle_vector(robot.angle_vector(), realtime_simulation=True)
+    interface.wait_interpolation()
+    print('==> end wait_interpolation')
+
+if __name__ == '__main__':
+    main()
